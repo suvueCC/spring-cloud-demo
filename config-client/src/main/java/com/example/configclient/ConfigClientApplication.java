79a141358ce6a5f8ef22ee30bf5b7bc35aa805d6
suvue <yj020827@163.com>
2020-06-09 16:58:28 +0800

    bus demo 尚未完成 以后有机会再补充 前面的是一下常用的

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -3,7 +3,14 @@ package com.example.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+/**
+ * 配置中心demo
+ *
+ * 服务端从git / github获取数据  客户端 从服务端拿数据
+ *
+ * @author suvue
+ * @date 2020/6/9
+ */
 @SpringBootApplication
 @EnableDiscoveryClient //配置为eureka服务发现的对象
 public class ConfigClientApplication {
